ed5da02acec195bf35c7f4fbd7d6ce4be6c06c0c
Joao4569 <johnnyrod4569@gmail.com>
2022-06-02 13:10:45 +0000

    Save progress at end of Adding Catergories section of tutorial

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ from taskmanager import app
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
-        port=os.environ.get("PORT"),
+        port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
