82ba04315518b2e876d069f94881e010f4a25ba3
Joao4569 <johnnyrod4569@gmail.com>
2022-06-02 19:01:18 +0000

    Save progress - end of day

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,4 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
   // select initialization
   let selects = document.querySelectorAll("select");
   M.FormSelect.init(selects);
+
+  // collapsible initializataion
+  let collapsibles = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(collapsibles);
 });
