2a56239f8ab912ee06068938bc67f810d3e82783
Joao4569 <johnnyrod4569@gmail.com>
2022-06-02 11:41:43 +0000

    Save progress end of Template inheritance section

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialization
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
