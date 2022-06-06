63686aecd882aac44e2d8fb8595142e727ca0ae8
Joao4569 <johnnyrod4569@gmail.com>
2022-06-06 11:29:59 +0000

    Update __init__.py - troubleshoot error on heroku, error db not defined and .startwith() not valid

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -15,7 +15,7 @@ else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri  #heroku
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)
 
