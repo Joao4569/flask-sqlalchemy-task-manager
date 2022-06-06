5176a3ee5b2a756f2b3c7b8c73423b826a935d39
Joao4569 <johnnyrod4569@gmail.com>
2022-06-06 10:35:46 +0000

    Save progress at end of Deleting Tasks section

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -72,3 +72,11 @@ def edit_task(task_id):
         task.category_id = request.form.get("category_id")
         db.session.commit()
     return render_template("edit_task.html", task=task, categories=categories)
+
+
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
